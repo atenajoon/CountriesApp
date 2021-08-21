7972d1332437d719cb4040f375aba76da6b6992d
Atena Razm <atenarazm@gmail.com>
2021-08-20 17:46:43 -0700

    countries array of objects defined

--- src/components/CountriesInput.jsx
+++ src/components/CountriesInput.jsx
@@ -1,6 +1,12 @@
 const CountriesInput = ({ onChange, value }) => {
+  const countries = [
+    { id: 1, name: "Iran" },
+    { id: 2, name: "Canada" },
+  ];
+
   return (
     <div>
+      {console.log(countries)}
       <label htmlFor="country">Choose Country: </label>
       <input id="country" onChange={onChange} value={value} />
       <h3>{value}</h3>
