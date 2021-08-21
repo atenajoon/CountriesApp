343712988079f861b1946ef1f1a51956dec37870
Atena Razm <atenarazm@gmail.com>
2021-08-20 17:28:35 -0700

    handleChange is getting the input value

--- src/components/CountriesApp.jsx
+++ src/components/CountriesApp.jsx
@@ -1,8 +1,17 @@
+import { useState, useEffect } from "react";
+
 const CountriesApp = () => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    console.log("changing", value);
+  };
+
   return (
     <div>
       <label htmlFor="country">Choose Country: </label>
-      <input id="country" />
+      <input id="country" onChange={handleChange} value={value} />
     </div>
   );
 };
